5448da8df2b72f7b5d54bfbab45c08364ea9f4c0
johannesvollmer <johannes.boenki@t-online.de>
2019-02-17 11:08:54 +0100

    Extract glium rendering to separate crate

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,10 @@ pub mod font;
 pub mod text;
 pub mod rectangle;
 
-#[cfg(feature = "glium-render")]
-pub mod glium_render;
-
 pub mod prelude {
     pub use crate::font::Font;
 //    pub use crate::text::TextCaretPositions;
 
-    #[cfg(feature = "glium-render")]
-    pub use crate::glium_render as gl;
 }
 
 
@@ -45,54 +40,5 @@ pub mod test {
         }
     }*/
 
-    #[cfg(feature = "glium-render")]
-    pub fn glium(font: crate::font::SerializedFont){
-        use glium::{glutin, Surface};
-        use crate::prelude::*;
-
-        let mut events_loop = glutin::EventsLoop::new();
-        let window = glutin::WindowBuilder::new();
-        let context = glutin::ContextBuilder::new();
-        let display = glium::Display::new(window, context, &events_loop).unwrap();
-
-        let font = Font::deserialized(font);
-        let font_texture = crate::glium_render::atlas_texture(&display, &font.atlas).unwrap();
-        let text_mesh = crate::glium_render::TextMesh::new(&display, &font, "Hello World").unwrap();
-        let solid_text_program = crate::glium_render::SolidTextProgram::new(&display).unwrap();
-
-        let mut closed = false;
-        while !closed {
-            let mut target = display.draw();
-            target.clear_color(0.0, 0.0, 0.1, 1.0);
-
-            {
-                let transform = [
-                    [1.0, 0.0, 0.0, 0.0],
-                    [0.0, 1.0, 0.0, 0.0],
-                    [0.0, 0.0, 1.0, 0.0],
-                    [0.0, 0.0, 0.0, 1.0],
-                ];
-
-                let fill = (1.0, 0.8, 0.2, 1.0);
-                let draw_parameters = glium::DrawParameters {
-                    ..Default::default()
-                };
-
-                solid_text_program.draw(&mut target, &font_texture, &text_mesh, fill, transform, &draw_parameters).unwrap();
-            }
-
-            target.finish().unwrap();
-
-            events_loop.poll_events(|ev| {
-                match ev {
-                    glutin::Event::WindowEvent { event, .. } => match event {
-                        glutin::WindowEvent::CloseRequested => closed = true,
-                        _ => (),
-                    },
-                    _ => (),
-                }
-            });
-        }
-    }
 
 }
